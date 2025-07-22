f6e52200d23d63e5d01164c310fb911e773b9158
Posiano Francis <Pocian98@gmail.com>
2025-07-22 13:38:16 +0300

    Simple calculator v2
    This version i added a do---while loop to continue prompting the user untill he quits

--- Calculator.c
+++ Calculator.c
@@ -2,48 +2,61 @@
 
 int main()
 {
-    printf(".......SIMPLE CALCULATOR.......\n\n");
-    double num1, num2, result;
-    char operator;
-
-    printf("Enter first number: ");
-    scanf("%lf", &num1);
-    printf("Enter second number: ");
-    scanf("%lf", &num2);
-    printf("Enter operator (+, -, *, /): ");
-    scanf(" %c", &operator);
-
-    if (operator == '+')
+    do
     {
-        result = num1 + num2;
-        printf("Result: %.2lf\n", result);
-    }
-    else if (operator == '-')
-    {
-        result = num1 - num2;
-        printf("Result: %.2lf\n", result);
-    }
-    else if (operator == '*')
-    {
-        result = num1 * num2;
-        printf("Result: %.2lf\n", result);
-    }
-    else if (operator == '/')
-    {
-        if (num2 != 0)
+        printf("Welcome to the Simple Calculator!\n");
+        printf("Press 'q' to quit or any other key to continue...\n");
+        char choice;
+        scanf(" %c", &choice);
+        if (choice == 'q')
         {
-            result = num1 / num2;
+            break;
+        }
+        double num1, num2, result;
+        char operator;
+
+        printf("Enter first number: ");
+        scanf("%lf", &num1);
+        printf("Enter second number: ");
+        scanf("%lf", &num2);
+        printf("Enter operator (+, -, *, /): ");
+        scanf(" %c", &operator);
+
+        if (operator == '+')
+        {
+            result = num1 + num2;
             printf("Result: %.2lf\n", result);
         }
+        else if (operator == '-')
+        {
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+        }
+        else if (operator == '*')
+        {
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+        }
+        else if (operator == '/')
+        {
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            }
+
+            else
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+        }
+
         else
         {
-            printf("Error: Division by zero is not allowed.\n");
+            printf("Error: Invalid operator.\n");
         }
-    }
-    else
-    {
-        printf("Error: Invalid operator.\n");
-    }
+
+    } while (1);
 
     return 0;
 }
